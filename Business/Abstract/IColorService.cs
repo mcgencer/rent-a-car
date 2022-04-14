4f65097ff803502b6df6e97bebeb80a2524e33a8
Nurettin Kemaloğlu <16290061@firat.edu.tr>
2022-04-14 18:10:04 +0300

    CRUD Operasyonlarının hazırlanması

--- Business/Abstract/IColorService.cs
+++ Business/Abstract/IColorService.cs
@@ -1,3 +1,4 @@
+using Entities.Concrete;
 using Core.Utilities.Result;
 using Entities.Concrete;
 using Entities.DTO;
@@ -6,11 +7,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Business.Abstract
 {
     public interface ICarService
     {
+        void Add(Car car);
+        void Update(Car car);
+        void Delete(Car car);
+        Car GetById(int id);
+        List<Car> GetAll();
+        List<Car> GetAllByBrandId(int brandId);
+        List<Car> GetAllByColorId(int colorId);
         IResult Add(Car car);
         IResult Update(Car car);
         IResult Delete(Car car);
