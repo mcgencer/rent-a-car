f730bcc7113159dc9f3275705a2c70c21e038100
Muratcan Gencer <74840999+mcgencer@users.noreply.github.com>
2022-04-22 09:51:17 +0300

    CarImages Tablosu Oluşturma
    Ek olarak RentalManager'de ufakbir kod bloğu silinip, düzeltildi.

--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -1,5 +1,6 @@
 using Business.Abstract;
 using Business.Constants;
+using Core.Utilities.Business;
 using Core.Utilities.Result;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -8,13 +9,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Business.Concrete
 {
     public class RentalManager : IRentalService
     {
         IRentalDal _rentalDal;
-
         public RentalManager(IRentalDal rentalDal)
         {
             _rentalDal = rentalDal;
@@ -23,44 +22,24 @@ namespace Business.Concrete
         public IResult Add(Rental rental)
         {
             var result=CheckIfTheCarHasBeenDelivered(rental);
+            var result=BusinessRules.Run(CheckIfTheCarHasBeenDelivered(rental));
             if (!result.Success) return result;
 
             _rentalDal.Add(rental);
             return new SuccessResult(Messages.Added);
         }
-
         public IResult Delete(Rental rental)
         {
             _rentalDal.Delete(rental);
             return new SuccessResult(Messages.Deleted);
         }
-
         public IDataResult<List<Rental>> GetAll()
         {
             var result = _rentalDal.GetAll();
             return new SuccessDataResult<List<Rental>>(result, Messages.Listed);
         }
-
         public IDataResult<Rental> GetById(int id)
         {
             var result = _rentalDal.Get(r => r.Id == id);
             return new SuccessDataResult<Rental>(result, Messages.Geted);
         }
-
-        public IResult Update(Rental rental)
-        {
-            _rentalDal.Update(rental);
-            return new SuccessResult(Messages.Updated);
-        }
-
-        private IResult CheckIfTheCarHasBeenDelivered(Rental rental)
-        {
-            var result = _rentalDal.Get(r => r.CarId == rental.CarId&&r.ReturnDate==null);
-            if (result != null)
-                if (rental.ReturnDate == null || rental.ReturnDate > result.RentDate)
-                    return new ErrorResult(Messages.TheCarHasNotBeenDeliveredYet);
-
-            return new SuccessResult();
-        }
-    }
-}
